e467fc01e2b58b8d8acd17c06ae4546a98a19c6c
Tobias Engel <tobias.engel@sevensides.net>
2020-02-05 15:24:05 +0000

    Added some visualization functionality

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app_global) {
     
-    var index 		= require('./routes/index')(app_global);
-	var nodes 		= require('./routes/node')(app_global);
-	var sensortypes = require('./routes/sensortypes')(app_global);
+    var index 		= require('./controllers/index')(app_global);
+	var nodes 		= require('./controllers/node')(app_global);
+	var sensortypes = require('./controllers/sensortypes')(app_global);
 	
     app_global.app.use('/', 			index);
 	app_global.app.use('/nodes', 		nodes);
